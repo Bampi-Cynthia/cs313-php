f4fddfdcfda3743dcbe6764530ba0df80fb774e5
Bampi-Cynthia <bam15003@byui.edu>
2018-05-03 13:58:45 -0600

    description

--- web/prove02 - personalpage.php
+++ web/prove02 - personalpage.php
@@ -11,11 +11,11 @@
 	<img class="photographer" src="IMG_5270.JPG" alt="photographer" />
 	<h1> About me</h1>
 	
-	<p>Hello! I am a sophomore at BYU-Idaho, studying Web Design and Web Development. I love photograph and design. I will be ready to graduate in december 2018.
+	<p>Hello! I am a senior at BYU-Idaho, studying Web Design and Web Development. I love photograph and design. I will be ready to graduate in december 2018.
 
-I am a mom of three beautiful kids, and married to my best friend for 10 years. We currently live in Utah, and a beautiful place to live, explore and photograph.
+I am a mom of three beautiful kids, and married to my best friend for 10 years. We currently live in Utah, it is a beautiful place to live, explore and photograph.
 
-My dream is one day to have my own business and photograph and designing. Hopefully you will like my work!</p>
+Hopefully you will like my work!</p>
 
 <?php
 date_default_timezone_set("America/Denver");
