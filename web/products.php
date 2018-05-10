ffff0673de8aee2e24c7ddfbd611ee5ac2306560
Bampi-Cynthia <bam15003@byui.edu>
2018-05-10 16:55:10 -0600

    cart

--- web/products.php
+++ web/products.php
@@ -8,9 +8,7 @@ $products = array(
   'label' => 'Dress',
   'image' =>'bluedress.jpg',
   'description' => 'Drop Waist Dress - The perfect dress to represent a 
-casually cool vibe that never goes out of style! The ocean 
-inspired stripes paired with the high/low hem culminate in 
-a look that promises to shine time and time again. $42 each'
+casually cool vibe that never goes out of style! $42 each'
   ),
   
   
