b61dde194dafa1db92548ea59355c95c35a3a0f3
Bampi-Cynthia <bam15003@byui.edu>
2018-05-23 19:03:10 -0600

    query change

--- web/week5/index.php
+++ web/week5/index.php
@@ -1,6 +1,18 @@
 <?php
 require('db.php');
-$stmt = $db->query('SELECT * FROM product');
+$stmt = $db->query("SELECT 
+product.id,
+product.name,
+category.name AS category_name,
+product.price,
+size.name AS size
+FROM product
+INNER JOIN category 
+ON product.category_id = category.id
+
+
+INNER JOIN size
+ON product.size_id = size.id;");
 while($row = $stmt->fetch()){
     echo '<pre>';
     var_dump($row);
