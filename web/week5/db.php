686655f7b70684ff2d99a2744112e13d522cc1b5
Bampi-Cynthia <bam15003@byui.edu>
2018-05-23 18:58:04 -0600

    code php fix

--- web/week5/db.php
+++ web/week5/db.php
@@ -1,8 +1,11 @@
 <?php
 $url = getenv('DATABASE_URL');
 $options = parse_url($url);
-list($host, $port, $user, $pass, $database) = $options;
-$database = ltrim($database, '/');
+$host = $options['host'];
+$port = $options['port'];
+$user = $options['user'];
+$pass = $options['pass'];
+$database = ltrim($options['path'], '/');
 $dsn = "pgsql:host={$host};port={$port};dbname={$database}";
 $db = new PDO($dsn, $user, $pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
