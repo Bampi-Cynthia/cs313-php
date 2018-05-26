0eb9442d7c45e681ecfdc4d6dce7df6cfe9cd88e
Bampi-Cynthia <35275795+Bampi-Cynthia@users.noreply.github.com>
2018-05-25 19:04:12 -0600

    Add files via upload

---
+++ web/week5/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <title> Clothing Store</title>
+    <style>
+    
+    </style>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Pacifico" rel="stylesheet">  
+    </head>
+    
+    <body>
+    <h1>Clothing Store</h1>
+
+<?php
+require('db.php');
+$stmt = $db->query("SELECT 
+product.id,
+product.name,
+category.name AS category_name,
+product.price,
+size.name AS size
+FROM product
+INNER JOIN category 
+ON product.category_id = category.id
+
+
+INNER JOIN size
+ON product.size_id = size.id;");
+while($row = $stmt->fetch()){
+   // echo '<pre>';//
+   // var_dump($row);//
+  //  echo '</pre>';//
+    echo '<h2>'. $row['name']."</h2>";
+    
+}
+        
+?>
+    </body>
+</html>
