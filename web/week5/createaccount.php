03b8d01eac36108824b4179e4cdbf64c33f4fd65
Bampi-Cynthia <bam15003@byui.edu>
2018-06-01 08:39:10 -0600

    log in and create account

---
+++ web/week5/createaccount.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>[Website Name] Style Guide</title>
+		<meta charset="UTF-8"/>
+    	<link href="styleguide.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<h3>It work</h3>
+	
+	</body>
+	</html>
+	
---
+++ web/week5/login.php
@@ -0,0 +1,28 @@
+<?php
+//log in page
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>[Website Name] Style Guide</title>
+		<meta charset="UTF-8"/>
+    	<link href="styleguide.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<form method="POST" action="createaccount.php">
+			<input type="text" placeholder="email"/>
+			<input type="text" placeholder="password"/>
+			<input type="button" value='Create Account'/>
+	
+	
+	
+		</form>
+	
+	
+	</body>
+	</html>
+	
+		
