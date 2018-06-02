e957b2ec131ad89c160e2bcb3af9964b18166b31
Bampi-Cynthia <bam15003@byui.edu>
2018-06-02 07:37:20 -0600

    images and prove assignment

--- web/week5/adphoto.sql
+++ web/week5/adphoto.sql
@@ -10,3 +10,15 @@ SELECT * FROM product WHERE name ='Floral Maxi Skirt';
 
 image_url = 'http://via.placeholder.com/500x500.png' WHERE name = '';
 
+UPDATE product SET
+
+description = 'double_peplum',
+image_url='double_peplum_top.jpg' WHERE name = 'Double Peplum Top';
+SELECT * FROM product WHERE name = 'Double Peplum Top';
+
+
+UPDATE product SET
+
+description = 'wonder tee',
+image_url='wondertee.jpg' WHERE name = 'Wonder Tee';
+SELECT * FROM product WHERE name = 'Wonder Tee';
