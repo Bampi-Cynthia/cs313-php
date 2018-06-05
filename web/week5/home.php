db012da81366a01f2f7bee704dae9846bf559cf8
Bampi-Cynthia <bam15003@byui.edu>
2018-06-05 14:41:02 -0600

    home page

---
+++ web/week5/home.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Modest Clothing Store</title>
+    <meta charset = "utf-8"/>
+    <link href="styleguide.css" type="text/css" rel="stylesheet" />
+
+	</head>
+<d class="content">
+	<div class="head">Get 10% Off - Use Code Wear</div>
+
+	<body>
+
+	<h2>Modest Clothing Store</h2>
+
+	<div>
+	<nav class="navbar"> 
+	 	<a href="web.html">Home</a>
+	 	<a href="#">Skirts</a>
+	 	<a href="#">Tops</a>
+	 	<a href="web1.html">Dresses</a>
+	 	<a href="#">Contact</a>
+	 	<a href="#" class="fa fa-shopping-bag"></a>
+	</nav>
+	</div>
+
+
+	<img src="http://shoplovelyinlace.com/wp-content/uploads/2016/11/Online-Modest-Clothing-Stores.jpg">
+
+
+	<div><p>New Arrivals</p></div>
+	<div class="pics">
+	<a href="#"><img src="https://cdn.shopify.com/s/files/1/0844/5511/products/DB18050055_1805_BlueStripe_Front_compact.jpg?v=1525274194" width: 600px;
+	height: 400px;></a>
+	<div class="desc">Drop Waist Dress</div>
+	<div class="desc">$42.99</div>
+	</div>
+
+
+	<div class="pics">
+	<a href="#"><img src="https://cdn.shopify.com/s/files/1/0844/5511/products/EM18056002_1805_White_Main_compact.jpg?v=1525274409" width: 600px;
+	height: 400px;></a>
+	<div class="desc">Double Peplum Top</div>
+	<div class="desc">$34.99</div>
+	</div>
+
+
+	<div class="pics">
+	<a href="#"><img src="https://cdn.shopify.com/s/files/1/0844/5511/products/EM18056001_1805_NavyWhite_Front_compact.jpg?v=1525215589" width: 600px;
+	height: 400px;></a>
+	<div class="desc">Ruffle Sleeve Blouse</div>
+	<div class="desc">$32.99</div>
+	</div>
+    
+    <div class="pics">
+	<a href="#"><img src="https://cdn.shopify.com/s/files/1/0844/5511/products/DB18040006_1804_Navy_M1_compact.jpg?v=1523918008g" width: 600px;
+	height: 400px;></a>
+	<div class="desc">Plaza perfect Skirt</div>
+	<div class="desc">$31.99</div>
+	</div>
+    
+    <div class="pics">
+	<a href="#"><img src="https://cdn.shopify.com/s/files/1/0844/5511/products/2018-3-1-DownEast-March-Web-475_compact.jpg?v=1520013294" width: 600px;
+	height: 400px;></a>
+	<div class="desc">Floral Skirt</div>
+	<div class="desc">$35.00</div>
+	</div>
+
+
+	<div class="shop"><button>Shop All</button></div>
+    
+
+	<div class="hr">
+	<hr>
+	<div class="hrtext">W-R</div>
+	</div>
+    
+	<div class="row">
+    
+
+	<div class="column-lside">
+	<ul>
+	<li><a href="web.html">Home</a></li>
+	<li><a href="#">Skirst</a></li>
+	<li><a href="#">Tops</a></li>
+	<li><a href="web1.html">Dresses</a></li>
+	<li><a href="#">Contact</a></li>
+	</ul>
+	</div>
+    
+	<div class="column-rside">
+	<ul>
+	<li><a href="#">Shipping</a></li>
+	<li><a href="#">Returns</a></li>
+	<li><a href="#">Store Policy</a></li>
+	<li><a href="#">Payment Methods</a></li>
+	<li><a href="#">FAQ</a></li>
+	</ul>
+	</div>
+
+	</div>
+</div>
+  
+	<div class="footer">
+	Join Our Mailing List
+	</div>
+  </html>
+  
+
+
+	<form  class="email2"  action="/action_page.php"">
+		E-mail:
+		<input type="email" name="email"> 
+		<input class="sub" class="button" type="submit" value="Submit">
+	</form>
+
+
+<div class="footer2"> 
+	&copy; 2018 Modest Clothing created by Cynthia Bampi
+	 </div>
