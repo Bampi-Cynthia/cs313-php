b50b0694cf1abc1c3ca807b9cb5b3bf7ace0ef7a
Bampi-Cynthia <bam15003@byui.edu>
2018-06-07 18:08:23 -0600

    one code

--- web/week5/browse.php
+++ web/week5/browse.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <!DOCTYPE html>
 <html>
 
@@ -74,80 +74,4 @@ while($row = $stmt->fetch()){
 ?>
         </div>
     </body>
-=======
-<!DOCTYPE html>
-<html>
 
-<head>
-    
-    <title> Clothing Store</title>
-    <style>
-    
-    </style>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat|Pacifico" rel="stylesheet">  
-    </head>
-    
-    <body>
-<?php
-require('db.php');
-$currentPage = 'browse.php';
-<nav>require('nav.php');</nav>
-?>
-
-
-<form action='browse.php' method='post'>
-<select name='category'>
-    <?php
-    $stmt = $db->query("SELECT id, name FROM category");
-    $stmt->execute();
-    while ($row = $stmt->fetch()) {
-        echo '<option value="'. $row['id'] . '">'.$row['name'] . '</option>';
-        
-    }
-    
-    ?>
-    
-    </select>
-<button>Search</button>
-</form>
-  <div id ='product'>
-      
-<?php
-if (isset($_POST['category'])) {
- $stmt = $db->prepare("SELECT 
-product.id,
-product.name,
-product.image_url,
-category.name AS category_name,
-product.price,
-size.name AS size
-FROM product
-INNER JOIN category 
-ON product.category_id = category.id
-
-
-INNER JOIN size
-ON product.size_id = size.id
-WHERE category.id = :cat_id
-;");
-    $stmt->bindValue('cat_id' , $_POST['category'],PDO::PARAM_INT );
-    
-    
-$stmt->execute();
-while($row = $stmt->fetch()){
-   // echo '<pre>';//
-   // var_dump($row);//
-  //  echo '</pre>';//
-    echo '<div class="product">';
-    echo '<h2>'. $row['name']."</h2>";
-    echo '<img src ="' . $row['image_url'] . '">';
-    echo '</div>';
-}
-}
-
-?>
-        </div>
-    </body>
->>>>>>> 49a80b2f2adae315ac4d08a66776ba3830fb6db6
-</html>
