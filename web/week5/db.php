9944dd3b41daacc80b991045c471d3e074c00636
Bampi-Cynthia <bam15003@byui.edu>
2018-06-07 18:25:09 -0600

    debug

--- web/week5/db.php
+++ web/week5/db.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 $url = getenv('DATABASE_URL');
 $options = parse_url($url);
@@ -10,16 +10,5 @@ $database = ltrim($options['path'], '/');
 $dsn = "pgsql:host={$host};port={$port};dbname={$database}";
 $db = new PDO($dsn, $user, $pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-=======
-<?php
-$url = getenv('DATABASE_URL');
-$options = parse_url($url);
-$host = $options['host'];
-$port = $options['port'];
-$user = $options['user'];
-$pass = $options['pass'];
-$database = ltrim($options['path'], '/');
-$dsn = "pgsql:host={$host};port={$port};dbname={$database}";
-$db = new PDO($dsn, $user, $pass);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
->>>>>>> 49a80b2f2adae315ac4d08a66776ba3830fb6db6
+
+?>
