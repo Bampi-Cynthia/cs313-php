aa14fc8362e35eaad009c3c6e85b20926e4b669b
Bampi-Cynthia <bam15003@byui.edu>
2018-06-07 18:48:24 -0600

    debug

--- web/week5/nav.php
+++ web/week5/nav.php
@@ -1,28 +1,18 @@
 
 <?php
 $links = array(
-        array(
-            'url =>'home.php',
-            'text' =>'Home'
-            ),
-    
-        array(
-            'url' =>'browse.php',
-            'text' =>'Clothes'
-            ),
-            
-            
-            
-            
-        
-            
-            array(
-            'url' =>'shopcart.php',
-            'text'=>'Shopping Cart'
-            )
-
-
-);
+array(
+'url' =>'home.php',
+'text' =>'Home'
+),   
+array(
+'url' =>'browse.php',
+'text' =>'Clothes'
+),           
+array(
+'url' =>'shopcart.php',
+'text'=>'Shopping Cart'
+));
 
 foreach($links as $link){
     echo '<a href="' . $link['url'] . '"';
