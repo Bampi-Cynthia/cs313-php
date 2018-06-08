d5c8444748b7c89fe2bf24ff45f1b995e61d4989
Bampi-Cynthia <bam15003@byui.edu>
2018-06-07 18:51:50 -0600

    debug

--- web/week5/db.php
+++ web/week5/db.php
@@ -1,15 +1,15 @@
 
 <?php
-echo "inside database";
-//$url = getenv('DATABASE_URL');
-//$options = parse_url($url);
-//$host = $options['host'];
-//$port = $options['port'];
-//$user = $options['user'];
-//$pass = $options['pass'];
-//$database = ltrim($options['path'], '/');
-//$dsn = "pgsql:host={$host};port={$port};dbname={$database}";
-//$db = new PDO($dsn, $user, $pass);
-//$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$url = getenv('DATABASE_URL');
+$options = parse_url($url);
+$host = $options['host'];
+$port = $options['port'];
+$user = $options['user'];
+$pass = $options['pass'];
+$database = ltrim($options['path'], '/');
+$dsn = "pgsql:host={$host};port={$port};dbname={$database}";
+$db = new PDO($dsn, $user, $pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
