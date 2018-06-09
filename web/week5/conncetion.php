0b9ef6a2fd1eed9738e51d1a38be88889cd07b55
Bampi-Cynthia <bam15003@byui.edu>
2018-06-09 12:59:27 -0600

    update

---
+++ web/week5/conncetion.php
@@ -0,0 +1,12 @@
+<<?php
+$url = getenv('DATABASE_URL');
+$options = parse_url($url);
+$host = $options['host'];
+$port = $options['port'];
+$user = $options['user'];
+$pass = $options['pass'];
+$database = ltrim($options['path'], '/');
+$dsn = "pgsql:host={$host};port={$port};dbname={$database}";
+$db = new PDO($dsn, $user, $pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
